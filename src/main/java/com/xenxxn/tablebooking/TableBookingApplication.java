3bcd0931602229ad3cb1fbaf9c80f7e24e1d0d43
zenxxn <bunbun2438@naver.com>
2023-09-18 19:01:12 +0900

    table-booking project

---
+++ src/main/java/com/xenxxn/tablebooking/TableBookingApplication.java
@@ -0,0 +1,13 @@
+package com.xenxxn.tablebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xenxxn/tablebooking/TableBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xenxxn.tablebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
