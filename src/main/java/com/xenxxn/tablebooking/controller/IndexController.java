53cc80e5d9d40e6659e18685d618e3f2000642bf
zenxxn <bunbun2438@naver.com>
2023-09-20 18:38:16 +0900

    test

--- src/main/java/com/xenxxn/tablebooking/controller/IndexController.java
+++ src/main/java/com/xenxxn/tablebooking/controller/IndexController.java
@@ -1,2 +1,10 @@
-package com.xenxxn.tablebooking.controller;public class IndexController {
+package com.xenxxn.tablebooking.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
 }
