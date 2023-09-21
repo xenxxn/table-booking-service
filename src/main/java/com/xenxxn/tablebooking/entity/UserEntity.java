043bed7ff0d6bb4d641485abc8e3c1d756ecaaea
zenxxn <bunbun2438@naver.com>
2023-09-21 23:33:07 +0900

    db 재설계

--- src/main/java/com/xenxxn/tablebooking/entity/UserEntity.java
+++ src/main/java/com/xenxxn/tablebooking/entity/UserEntity.java
@@ -1,8 +1,11 @@
 package com.xenxxn.tablebooking.entity;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Getter
 @Setter
@@ -12,14 +15,38 @@ import javax.persistence.*;
 @ToString
 @Table(name = "USER")
 @Entity
-public class User {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
     private Long id;
-
     private String username;
     private String password;
     private String name;
-    private Ro
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
 }
